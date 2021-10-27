8b9825f2c917632442d1e18ae5a1e2c1b61bb26a
proloypaul <proloyp23@gmail.com>
2021-10-28 01:17:34 +0600

    set route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Admin from './Component/Admin/Admin';
+import Donation from './Component/Donation/Donation';
+import Events from './Component/Events/Events';
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import Register from './Component/Register/Register';
 
 function App() {
   return (
     <div>
-      <h1>WOw volunteer network</h1>
+      <Router>
+        <Header></Header>
+      <Switch>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route path="/donation">
+          <Donation></Donation>
+        </Route>
+        <Route path="/event">
+          <Events></Events>
+        </Route>
+        <Route path="/register">
+          <Register></Register>
+        </Route>
+        <Route path="/admin">
+          <Admin></Admin>
+        </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
         <div>
             <h1>Header section</h1>
+            <nav>
+                <div>
+                    
+                </div>
+                <div>
+                    <NavLink to="/home">Home</NavLink>
+                    <NavLink to="/donation">Donation</NavLink>
+                    <NavLink to="/event">Events</NavLink>
+                    <NavLink to="/register">Register</NavLink>
+                    <NavLink to="/admin">Admin</NavLink>
+                </div>
+            </nav>
         </div>
     );
 };
